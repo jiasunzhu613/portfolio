3501e9e29c081796b4661a0310cecbc92a281987
jonathanzhu <jiasunzhu@gmail.com>
2025-08-05 23:52:20 -0400

    webring component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ import {
   LinkedInLogoIcon,
   FileTextIcon,
 } from '@radix-ui/react-icons'
+
+const filter: string =
+  'brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(0%) hue-rotate(301deg) brightness(103%) contrast(102%)'
+
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
@@ -26,17 +30,18 @@ export default function Home() {
               In my spare time, I like to draw, play sports (tennis mainly) and
               eat some good food.
             </h3>
-            <div className="mt-10 flex items-center">
-              <Image
-                src="/profileImage_new.jpg"
-                width={100}
-                height={100}
-                alt="Picture of the author"
-                objectFit="cover"
-                objectPosition="center"
-                className="rounded-full mr-3"
-              />
-              <div className="flex flex-col gap-3 mr-3">
+            <div className="flex justify-between mt-4">
+              <div className="flex">
+                <Image
+                  src="/profileImage_new.jpg"
+                  width={64}
+                  height={64}
+                  alt="Picture of the author"
+                  objectFit="cover"
+                  objectPosition="center"
+                  className="rounded-full mr-3 border-2 outline outline-white"
+                />
+
                 <Link
                   href="https://github.com/jiasunzhu613"
                   target="_blank"
@@ -45,11 +50,9 @@ export default function Home() {
                   <GitHubLogoIcon
                     width={20}
                     height={20}
-                    className="mr-1"
+                    className="mr-1 group-hover:size-6 duration-200 transition-all"
                   ></GitHubLogoIcon>
-                  <span className="text-[1em] opacity-0 transition-all ease-in-out duration-300 group-hover:ml-1 group-hover:opacity-100">
-                    Github
-                  </span>
+                  <span className="text-[1em] ">Github</span>
                 </Link>
 
                 <Link
@@ -60,12 +63,10 @@ export default function Home() {
                   <LinkedInLogoIcon
                     width={20}
                     height={20}
-                    className="mr-1"
+                    className="mr-1 group-hover:size-6 duration-200 transition-all"
                     // transition ease-in-out duration-300 group-hover:scale-110 group-hover:-translate-y-1
                   ></LinkedInLogoIcon>
-                  <span className="text-[1em] opacity-0 transition-all ease-in-out duration-300 group-hover:ml-1 group-hover:opacity-100">
-                    Linkedin
-                  </span>
+                  <span className="text-[1em] ">Linkedin</span>
                 </Link>
 
                 <Link
@@ -76,12 +77,36 @@ export default function Home() {
                   <FileTextIcon
                     width={20}
                     height={20}
-                    className="mr-1"
+                    className="mr-1 group-hover:size-6 duration-200 transition-all"
                   ></FileTextIcon>
-                  <span className="text-[1em] opacity-0 transition-all ease-in-out duration-300 group-hover:ml-1 group-hover:opacity-100">
-                    Resume
-                  </span>
+                  <span className="text-[1em]">Resume</span>
                 </Link>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <a
+                  className="text-xl font-mono text-gray-200 hover:text-white"
+                  href="https://uoftwebring.com/redirect?nav=prev&id=10"
+                >
+                  ←
+                </a>
+                <a href="https://uoftwebring.com" target="_blank">
+                  <Image
+                    src="https://uoftwebring.com/ring_logo.svg"
+                    alt="UofT Webring"
+                    height={24}
+                    width={24}
+                    style={{
+                      filter: filter,
+                    }}
+                  />
+                </a>
+                <a
+                  className="text-xl font-mono text-gray-200 hover:text-white"
+                  href="https://uoftwebring.com/redirect?nav=next&id=10"
+                >
+                  →
+                </a>
               </div>
             </div>
           </div>
